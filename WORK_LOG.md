f68dc72c7dca7c368adac2824a7e176dc0492e68
codykletter <cody.kletter@menloschool.org>
2025-03-12 14:55:10 -0700

    added tabulation lev

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
-Name: YOUR NAME HERE
+Name: Cody
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date     |    Time     |                                                                                                                                                                                          Update |
+|:---------|:-----------:|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| April 5  |   9-10pm    |                                                                                                                                                      I tried to write abstract, efficient code. |
+| March 12 | 2:30-2:53pm | I spent this time writing the tabulation edit distance based off the lecture in class. I wasn't sure where we should find threshold so I made it an instance variable in the autocorrect class. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -1,6 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.params.shadow.com.univocity.parsers.common.NormalizedString.toArray;
 
 /**
  * Autocorrect
@@ -8,30 +12,60 @@ import java.io.IOException;
  * A command-line tool to suggest similar words when given one not in the dictionary.
  * </p>
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Cody Kletter
  */
 public class Autocorrect {
-
+    private int threshold;
+    private String[] words;
     /**
      * Constucts an instance of the Autocorrect class.
      * @param words The dictionary of acceptable words.
      * @param threshold The maximum number of edits a suggestion can have.
      */
     public Autocorrect(String[] words, int threshold) {
-
+        this.threshold = threshold;
+        this.words = words;
     }
 
     /**
      * Runs a test from the tester file, AutocorrectTester.
      * @param typed The (potentially) misspelled word, provided by the user.
      * @return An array of all dictionary words with an edit distance less than or equal
-     * to threshold, sorted by edit distnace, then sorted alphabetically.
+     * to threshold, sorted by edit distance, then sorted alphabetically.
      */
     public String[] runTest(String typed) {
-
-        return new String[0];
+        ArrayList<String> similar = new ArrayList<String>();
+        for (String word : words) {
+            if (editDistance(word, typed) <= threshold) {
+                similar.add(word);
+            }
+        }
+        String[] words = new String[similar.size()];
+        words = similar.toArray(new String[0]);
+        return words;
     }
-
+    public int editDistance(String typed, String dict) {
+        int[][] lev = new int[typed.length() + 1][dict.length() + 1];
+        // Fill first row with 0, 1, 2...
+        for (int i = 0; i < lev[0].length; i++) {
+            lev[0][i] = i;
+        }
+        // Fill first col with 0, 1, 2...
+        for (int j = 0; j < lev.length; j++) {
+            lev[j][0] = j;
+        }
+        for (int i = 1; i < lev.length; i++) {
+            for (int j = 1; j < lev[0].length; j++) {
+                if (typed.charAt(i - 1) == dict.charAt(j - 1)) {
+                    lev[i][j] = lev[i - 1][j - 1];
+                }
+                else {
+                    lev[i][j] = Math.min(Math.min(lev[i - 1][j], lev[i][j - 1]), lev[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+        return lev[typed.length()][dict.length()];
+    }
 
     /**
      * Loads a dictionary of words from the provided textfiles in the dictionaries directory.
