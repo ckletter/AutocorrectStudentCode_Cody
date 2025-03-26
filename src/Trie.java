e44f25cf5fb90ee7f517d3d77026c46c36c82394
codykletter <cody.kletter@menloschool.org>
2025-03-25 21:17:14 -0700

    more dfs work

--- src/Trie.java
+++ src/Trie.java
@@ -7,16 +7,24 @@ public class Trie {
     private final Prefix root;
     private int threshold;
     private int[] letterMap;
+    private int[] toIndexMap;
     public Trie(int threshold) {
         root = new Prefix();
         this.threshold = threshold;
         this.letterMap = new int[RADIX];
+        this.toIndexMap = new int[256];
         // Fill letter map with letters in alphabet
         for (int i = 0; i < RADIX - 1; i++) {
             this.letterMap[i] = i + 'a';
         }
         // Set the last index of the trie equal to the ' ascii
         this.letterMap[RADIX - 1] = 39;
+        // Fill to index map with letters in alphabet
+        for (int i = 0; i < RADIX - 1; i++) {
+            this.toIndexMap[i + 'a'] = i;
+        }
+        // Set the last index of the trie equal to the ' ascii
+        this.toIndexMap[39] = RADIX - 1;
     }
     private class Prefix {
         private boolean isWord;
@@ -49,46 +57,44 @@ public class Trie {
             Prefix[] currentSuffixes = currentPrefix.getChildren();
             // If that prefix does not exist in the currentPrefix's children
             // Create new child of that prefix with current letter added
-            if (currentSuffixes[letterIndex] == null) {
-                currentSuffixes[letterIndex] = new Prefix();
+            if (currentSuffixes[toIndexMap[letterIndex]] == null) {
+                currentSuffixes[toIndexMap[letterIndex]] = new Prefix();
             }
             // Go to the location of that newly created prefix
-            currentPrefix = currentSuffixes[letterIndex];
+            currentPrefix = currentSuffixes[toIndexMap[letterIndex]];
         }
         // Once at the end of the current word, set that prefix to existing in the dictionary
         currentPrefix.setWord();
     }
 
     public void editDistDFS(Prefix currentPrefix, String word, String currentString, int index, ArrayList<String> possibleWords, int edits) {
-        if (edits == threshold) {
+        // If more edits made than threshold, prune branch
+        if (edits > threshold) {
             return;
         }
-        // Follow rest of word and add relevant existing words within threshold
-        if (edits != 0) {
-            followRestOfWord(currentPrefix, word, currentString, index, possibleWords);
+        // Prune branch if prefix does not exist in dictionary
+        if (currentPrefix == null) {
+            return;
+        }
+        // If valid word, add to our possible words
+        if (index == word.length() && currentPrefix.isWord() && !currentString.equals(word)) {
+            possibleWords.add(currentString);
+        }
+        if (index < word.length()) {
+            // Case where nothing is changed, follows the word down
+            editDistDFS(currentPrefix, word, currentString + word.charAt(index), index + 1, possibleWords, edits);
+            // Try all possible substitutions
+            for (int j = 0; j < RADIX; j++) {
+                editDistDFS(currentPrefix, word, currentString + letterMap[j], index + 1, possibleWords, edits + 1);
+            }
         }
         for (int j = 0; j < RADIX; j++) {
             Prefix currentChild = currentPrefix.getChildren()[j];
             // Addition
             editDistDFS(currentChild, word, currentString + letterMap[j], index, possibleWords, edits + 1);
-            // Deletion
-            editDistDFS(currentChild, word, currentString, index + 1, possibleWords, edits + 1);
-            // Substitution
-            editDistDFS(currentPrefix, word, currentString + letterMap[j], index - 1, possibleWords, edits + 1);
         }
-        // Case where nothing is changed, follows the word down
-        editDistDFS(currentPrefix, word, currentString + word.charAt(index), index + 1, possibleWords, edits);
-    }
-    public void followRestOfWord(Prefix current, String word, String currentString, int index, ArrayList<String> possibleWords) {
-        StringBuilder currentStringBuilder = new StringBuilder(currentString);
-        for (int j = index; j < word.length(); j++) {
-            current = current.getChildren()[word.charAt(index) - 'a'];
-            currentStringBuilder.append(word.charAt(index) - 'a');
-        }
-        currentString = currentStringBuilder.toString();
-        if (current.isWord() && !currentString.equals(word)) {
-            possibleWords.add(currentString);
-        }
+        // Deletion
+        editDistDFS(currentPrefix, word, currentString, index + 1, possibleWords, edits + 1);
     }
     public void findCandidates(String word) {
             ArrayList<String> possibleWords = new ArrayList<>();
